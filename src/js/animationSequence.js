8a329c3961f6a8ee3ba24358c0caa406729c267f
Ben Boyle <benjamins.boyle@gmail.com>
2014-09-23 18:41:25 +1000

    use requestAnimationFrame
    - replace timeouts with requestAnimationFrame
    - change model to use duration instead of timeout (durations are offet
    from each other, the timeouts all shared the same origin time)
    - handle toggling multiple classes

--- src/js/animationSequence.js
+++ src/js/animationSequence.js
@@ -3,43 +3,35 @@
 
 	var media = document.getElementById( 'media' );
 	var sequence = [
-		{ timeout:  100, on: 'thumbnail', off: 'fade-in'   },
-		{ timeout:  700, on: 'full',      off: 'thumbnail' },
-		{ timeout: 4000, on: 'fade-in',   off: 'full'      },
-		{ timeout: 4400, on: 'thumbnail', off: 'full'      },
+		{ duration:  100, on: 'thumbnail',         off: 'fade-in'   },
+		{ duration:  600, on: 'full',              off: 'thumbnail' },
+		{ duration: 3500, on: 'fade-in thumbnail', off: 'full'      },
+		{ duration:  700, on: 'thumbnail',         off: 'full'      },
 	];
 
-	// TODO use requestAnimationFrame
-	// http://codepen.io/bboyle/pen/gAuwv?editors=001
 
-	function run() {
-		// .1s-.4s: fade in
-		setTimeout(function() {
-			media.classList.add( sequence[ 0 ].on );
-			media.classList.remove( sequence[ 0 ].off );
-		}, sequence[ 0 ].timeout );
+	var i = 0;
+	var then = Date.now();
+	requestAnimationFrame(function animateSequence() {
+		var now = Date.now();
 
-		// .7-1.4s: enlarge
-		setTimeout(function() {
-			media.classList.add( sequence[ 1 ].on );
-			media.classList.remove( sequence[ 1 ].off );
-		}, sequence[ 1 ].timeout );
+		// time to update?
+		if ( now - then >= sequence[ i ].duration ) {
+			// update classes
+			sequence[ i ].on.split( ' ' ).forEach(function( newClass ) {
+				media.classList.add( newClass );
+			});
+			sequence[ i ].off.split( ' ' ).forEach(function( oldClass ) {
+				media.classList.remove(  oldClass );
+			});
 
-		// 4-4.3s: fade-out
-		setTimeout(function() {
-			media.classList.add( sequence[ 2 ].on );
-			media.classList.remove( sequence[ 2 ].off );
-		}, sequence[ 2 ].timeout );
+			// next in sequence
+			then = now;
+			i = ( i + 1 ) % sequence.length;
+		}
 
-		// 4.4s: loop
-		setTimeout(function() {
-			media.classList.add( sequence[ 3 ].on );
-			media.classList.remove( sequence[ 3 ].off );
-			run();
-		}, sequence[ 3 ].timeout );
-	}
-
-	// go!
-	run();
+		// loop
+		requestAnimationFrame( animateSequence );
+	});
 
 }());
