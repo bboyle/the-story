37978b8b4e1d5161b9636b3a8f0150341170844b
Ben Boyle <benjamins.boyle@gmail.com>
2014-09-26 00:36:04 +1000

    cycle through 5 sample images
    - add sample images
    - change default image (in html)

--- src/js/animationSequence.js
+++ src/js/animationSequence.js
@@ -1,6 +1,19 @@
 (function() {
 	'use strict';
 
+	var images = [
+		// <a href="https://www.flickr.com/photos/expomeloneras/6081715867" title="Ballet de Moscú 2011 by ExpoMeloneras, on Flickr"><img src="https://farm7.staticflickr.com/6066/6081715867_b131bb9298.jpg" width="500" height="232" alt="Ballet de Moscú 2011"></a>
+		'https://farm7.staticflickr.com/6066/6081715867_b131bb9298.jpg',
+		// <a href="https://www.flickr.com/photos/greg_photos/8708749551" title="Aurora_Ballet_20120504_201 by Greg Gamble, on Flickr"><img src="https://farm9.staticflickr.com/8557/8708749551_688f8f57ff.jpg" width="500" height="333" alt="Aurora_Ballet_20120504_201"></a>
+		'https://farm9.staticflickr.com/8557/8708749551_688f8f57ff.jpg',
+		// <a href="https://www.flickr.com/photos/melillamirada/4121521415" title="Ballet en la Gala Lírica by Miguel, on Flickr"><img src="https://farm3.staticflickr.com/2609/4121521415_45d78b2cbd.jpg" width="500" height="436" alt="Ballet en la Gala Lírica"></a>
+		'https://farm3.staticflickr.com/2609/4121521415_45d78b2cbd.jpg',
+		// <a href="https://www.flickr.com/photos/panacheart/5159389818" title="Ballet Bellevue Nutcracker 2010 by Scott Moore, on Flickr"><img src="https://farm2.staticflickr.com/1334/5159389818_c97c7faff9.jpg" width="364" height="500" alt="Ballet Bellevue Nutcracker 2010"></a>
+		'https://farm2.staticflickr.com/1334/5159389818_c97c7faff9.jpg',
+		// <a href="https://www.flickr.com/photos/lac-bac/8054555591" title="Seven dancers performing in &quot;Sur les Pointes,&quot; a production by Boris Volkoff&#x27;s Canadian Ballet / Prestation « Sur les pointes » de sept danseuses, dans une production des ballets canadiens de Boris Volkoff by Library and Archives Canada / Bibliothèque et Archives Canada, on Flickr"><img src="https://farm9.staticflickr.com/8040/8054555591_686a8ebbc4.jpg" width="500" height="377" alt="Seven dancers performing in &quot;Sur les Pointes,&quot; a production by Boris Volkoff&#x27;s Canadian Ballet / Prestation « Sur les pointes » de sept danseuses, dans une production des ballets canadiens de Boris Volkoff"></a>
+		'https://farm9.staticflickr.com/8040/8054555591_686a8ebbc4.jpg',
+	];
+
 	var media = document.getElementById( 'media' );
 	var sequence = [
 		{ duration:  100, on: 'thumbnail',         off: 'fade-in'   },
@@ -28,6 +41,11 @@
 			// next in sequence
 			then = now;
 			i = ( i + 1 ) % sequence.length;
+
+			// new media?
+			if ( i === 0 ) {
+				media.getElementsByTagName( 'img' )[ 0 ].src = images[ Math.floor( Math.random() * images.length ) ];
+			}
 		}
 
 		// loop
