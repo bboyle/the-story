8d5652e8cacd881cf0c6d2b132cf4c8f7c0fdaf5
Ben Boyle <benjamins.boyle@gmail.com>
2014-09-26 00:45:23 +1000

    add simple image preloading

--- src/js/animationSequence.js
+++ src/js/animationSequence.js
@@ -13,6 +13,7 @@
 		// <a href="https://www.flickr.com/photos/lac-bac/8054555591" title="Seven dancers performing in &quot;Sur les Pointes,&quot; a production by Boris Volkoff&#x27;s Canadian Ballet / Prestation « Sur les pointes » de sept danseuses, dans une production des ballets canadiens de Boris Volkoff by Library and Archives Canada / Bibliothèque et Archives Canada, on Flickr"><img src="https://farm9.staticflickr.com/8040/8054555591_686a8ebbc4.jpg" width="500" height="377" alt="Seven dancers performing in &quot;Sur les Pointes,&quot; a production by Boris Volkoff&#x27;s Canadian Ballet / Prestation « Sur les pointes » de sept danseuses, dans une production des ballets canadiens de Boris Volkoff"></a>
 		'https://farm9.staticflickr.com/8040/8054555591_686a8ebbc4.jpg',
 	];
+	var nextImage = new Image();
 
 	var media = document.getElementById( 'media' );
 	var sequence = [
@@ -38,13 +39,19 @@
 				media.classList.remove(  oldClass );
 			});
 
+
+			// prepare next image
+			if ( i === 0 ) {
+				nextImage.src = images[ Math.floor( Math.random() * images.length ) ];
+			}
+
 			// next in sequence
 			then = now;
 			i = ( i + 1 ) % sequence.length;
 
 			// new media?
 			if ( i === 0 ) {
-				media.getElementsByTagName( 'img' )[ 0 ].src = images[ Math.floor( Math.random() * images.length ) ];
+				media.getElementsByTagName( 'img' )[ 0 ].src = nextImage.src;
 			}
 		}
 
